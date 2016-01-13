dbbfcf14c2ba02b08cb5d67aa97db468af79fa2e
Shashank-Reddy <vemula.shashank@gmail.com>
2016-01-13 20:49:32 +0530

    working json in 8.3.0

--- app/src/main/java/sara/locus/Login_Activity.java
+++ app/src/main/java/sara/locus/Login_Activity.java
@@ -12,6 +12,9 @@ public class Login_Activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+
+
     }
 
     @Override
